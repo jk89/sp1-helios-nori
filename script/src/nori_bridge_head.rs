42c0f5b7a6f01e7367d3c9c382fe20387016c2d9
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2025-02-07 12:16:36 +0000

    Working on bridge head startup procedure

--- script/src/nori_bridge_head.rs
+++ script/src/nori_bridge_head.rs
@@ -1,4 +1,7 @@
 use alloy_primitives::FixedBytes;
+use helios_consensus_core::consensus_spec::MainnetConsensusSpec;
+use helios_ethereum::consensus::Inner;
+use helios_ethereum::rpc::http_rpc::HttpRpc;
 use reqwest::Url;
 use serde_cbor::ser;
 use sp1_sdk::{EnvProver, ProverClient, SP1ProofWithPublicValues, SP1ProvingKey, SP1Stdin};
@@ -19,10 +22,11 @@ pub struct NoriBridgeHead {
     slot: u64,
     nb_checkpoint_location: String,
     rpc_url: Url,
+    client: Inner<MainnetConsensusSpec, HttpRpc>
     /*
 
     checkpoint: FixedBytes<32>,
-    client: EnvProver*/
+    */
 }
 
 impl NoriBridgeHead {
@@ -32,44 +36,56 @@ impl NoriBridgeHead {
             .expect("DEST_RPC_URL not set")
             .parse()
             .unwrap();
-        let slot: u64 = env::var("COLD_START_HEAD")
+        let mut slot: u64 = env::var("COLD_START_HEAD")
             .expect("COLD_START_HEAD not set")
             .parse()
             .unwrap(); // e.g. 11008128
         let nb_checkpoint_location =
             env::var("NORI_BRIGE_CHECKPOINT_FILE").expect("NORI_BRIGE_CHECKPOINT_FILE not set");
 
-        let mut nbh = Self {
-            slot,
-            nb_checkpoint_location,
-            rpc_url,
-        };
-
-         // Warm start procedure
-        if nbh.nb_checkpoint_exists() {
-           info!("Loading nori slot checkpoint from file.");
-           let nb_checkpoint = nbh.load_nb_checkpoint();
-           nbh.slot = nb_checkpoint.slot;
-        }
-        else {
+        // Warm start procedure
+        if NoriBridgeHead::nb_checkpoint_exists(&nb_checkpoint_location) {
+            info!("Loading nori slot checkpoint from file.");
+            let nb_checkpoint = NoriBridgeHead::load_nb_checkpoint(&nb_checkpoint_location);
+            slot = nb_checkpoint.slot;
+        } else {
             info!("Resorting to COLD_START_HEAD checkpoint env var.");
         }
 
         // Get beacon checkpoint
-        let beacon_checkpoint = get_checkpoint(nbh.slot).await;
+        let beacon_checkpoint = get_checkpoint(slot).await;
 
-        // Get the client from the checkpoint
+        // Get the client from the beacon checkpoint
         let client = get_client(beacon_checkpoint).await;
 
         //  //nbh.save_checkpoint();
-
-        nbh
+        Self {
+            slot,
+            nb_checkpoint_location,
+            rpc_url,
+            client,
+        }
     }
 
+    // Static method to check if the checkpoint file exists
+    pub fn nb_checkpoint_exists(nb_checkpoint_location: &str) -> bool {
+        Path::new(nb_checkpoint_location).exists()
+    }
 
-    // Function to check if the checkpoint file exists
-    fn nb_checkpoint_exists(&self) -> bool {
-        Path::new(&self.nb_checkpoint_location).exists()
+    // Static method to load the checkpoint from file
+    pub fn load_nb_checkpoint(nb_checkpoint_location: &str) -> NoriBridgeCheckpoint {
+        // Open the checkpoint file
+        let mut file =
+            File::open(nb_checkpoint_location).expect("Failed to open nori checkpoint file.");
+
+        // Read the contents into a Vec<u8>
+        let mut serialized_checkpoint = Vec::new();
+        file.read_to_end(&mut serialized_checkpoint)
+            .expect("Failed to read nori checkpoint file.");
+
+        let nb_checkpoint: NoriBridgeCheckpoint = serde_cbor::from_slice(&serialized_checkpoint)
+            .expect("Failed to deserialize nori checkpoint");
+        nb_checkpoint
     }
 
     pub fn save_nb_checkpoint(&self) {
@@ -80,23 +96,11 @@ impl NoriBridgeHead {
             serde_cbor::to_vec(&checkpoint).expect("Failed to serialize nori checkpoint");
 
         // Write the serialized data to the file specified by `checkpoint_location`
-        let mut file =
-            File::create(&self.nb_checkpoint_location).expect("Failed to create nori checkpoint file.");
+        let mut file = File::create(&self.nb_checkpoint_location)
+            .expect("Failed to create nori checkpoint file.");
         file.write_all(&serialized_nb_checkpoint)
             .expect("Failed to write to checkpoint file.");
 
         info!("Nori bridge checkpoint saved successfully.");
-    }
-
-    fn load_nb_checkpoint(&self) -> NoriBridgeCheckpoint {
-        // Open the checkpoint file
-        let mut file = File::open(&self.nb_checkpoint_location).expect("Failed to open nori checkpoint file.");
-
-        // Read the contents into a Vec<u8>
-        let mut serialized_checkpoint = Vec::new();
-        file.read_to_end(&mut serialized_checkpoint).expect("Failed to read nori checkpoint file.");
-
-        let nb_checkpoint: NoriBridgeCheckpoint = serde_cbor::from_slice(&serialized_checkpoint).expect("Failed to deserialize nori checkpoint");
-        nb_checkpoint
     }
 }
